ae89cd364ce7ebb54b4ff5b638c08da5a18f0e7c
Deddy Wolley <deddywolley@gmail.com>
2021-03-02 10:57:21 +0800

    update addTodo Modal

--- components/Todo/AddTodoBtn.jsx
+++ components/Todo/AddTodoBtn.jsx
@@ -1,11 +1,12 @@
-import { IconButton, useDisclosure } from "@chakra-ui/react";
-import { useRef } from "react";
-import { AddIcon } from "../Icons";
-import AddTodoModal from "./AddTodoModal";
+import { IconButton, useDisclosure } from "@chakra-ui/react"
+import { useRef } from "react"
+import { AddIcon } from "../Icons"
+import AddTodoModal from "./AddTodoModal"
+import TodoForm from "./TodoForm"
 
 const AddTodoBtn = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const initialRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = useRef()
   return (
     <>
       <IconButton
@@ -17,9 +18,15 @@ const AddTodoBtn = () => {
         borderRadius="999px"
         bg="brand.600"
       />
-      <AddTodoModal isOpen={isOpen} initialRef={initialRef} onClose={onClose} />
+      <AddTodoModal
+        isOpen={isOpen}
+        label="Add Todo"
+        initialRef={initialRef}
+        onClose={onClose}
+        render={<TodoForm initialRef={initialRef} onClose={onClose} px="0" />}
+      />
     </>
-  );
-};
+  )
+}
 
-export default AddTodoBtn;
+export default AddTodoBtn
