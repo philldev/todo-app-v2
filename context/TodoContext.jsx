3b0d6f17285c2a6d3b5cf3e7f837400871ed24c0
Deddy Wolley <deddywolley@gmail.com>
2021-03-02 10:57:05 +0800

    added addSubtodo fn

--- context/TodoContext.jsx
+++ context/TodoContext.jsx
@@ -1,37 +1,69 @@
-import { createContext } from "react";
-import shortid from "shortid";
-import useLocalStorage from "../hooks/useLocalStorage";
+import { createContext } from "react"
+import shortid from "shortid"
+import useLocalStorage from "../hooks/useLocalStorage"
 
-export const TodoContext = createContext();
+export const TodoContext = createContext()
 
 export function TodoProvider({ children }) {
-  const [list, setList] = useLocalStorage("todo-list", []);
+  const [list, setList] = useLocalStorage("todo-list", [])
 
   const addTodo = (text) => {
     if (text) {
-      setList([...list, { id: shortid(), text, isDone: false }]);
+      setList([
+        ...list,
+        {
+          id: shortid(),
+          text,
+          isDone: false,
+          createdAt: new Date().toISOString(),
+          subTodo: [],
+        },
+      ])
     }
-  };
+  }
+
+  const addSubTodo = (todoId, text) => {
+    if (text) {
+      setList((prev) =>
+        prev.map((item) =>
+          item.id === todoId
+            ? {
+                ...item,
+                subTodo: [
+                  ...item.subTodo,
+                  {
+                    id: shortid(),
+                    text,
+                    isDone: false,
+                    createdAt: new Date().toISOString(),
+                  },
+                ],
+              }
+            : item
+        )
+      )
+    }
+  }
 
   const deleteTodo = (item) => {
-    setList(list.filter((i) => i.id !== item.id));
-  };
+    setList(list.filter((i) => i.id !== item.id))
+  }
 
   const checkTodo = (item) => {
     setList((prev) =>
       prev.map((i) => (i.id === item.id ? { ...i, isDone: !i.isDone } : i))
-    );
-  };
+    )
+  }
 
   const deleteAllCompleted = () => {
-    setList(list.filter((i) => !i.isDone));
-  };
+    setList(list.filter((i) => !i.isDone))
+  }
 
   const editTodo = (todo) => {
-    setList((prev) => prev.map((i) => (i.id === todo.id ? todo : i)));
-  };
+    setList((prev) => prev.map((i) => (i.id === todo.id ? todo : i)))
+  }
 
-  const completeCount = list.filter((i) => i.isDone).length;
+  const completeCount = list.filter((i) => i.isDone).length
 
   return (
     <TodoContext.Provider
@@ -47,5 +79,5 @@ export function TodoProvider({ children }) {
     >
       {children}
     </TodoContext.Provider>
-  );
+  )
 }
